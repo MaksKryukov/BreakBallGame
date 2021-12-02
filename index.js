f1b51d822e5b0c5a48009051d281da8f12b3023d
dunice practice <dunicepractice@Mac-mini-Dunice-103.local>
2021-12-02 11:40:35 +0300

    added objects and fixed the game zone

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+var isNotCreated=true;
+var isGameActive=false;
+
+const timeToFrame = 100;
+
 document.addEventListener('readystatechange', () => {
     if (document.readyState === 'complete') main();
 });
@@ -31,7 +36,7 @@ function main() {
         pages.index.classList.remove('hidden');
         pages.game.classList.add('hidden');
         pages.highscore.classList.add('hidden');
-        game();
+        isGameActive=false;
     })
 
     const gameButtonElement = document.getElementById('play-btn');
@@ -40,17 +45,55 @@ function main() {
         pages.index.classList.add('hidden');
         pages.game.classList.remove('hidden');
         pages.highscore.classList.add('hidden');
+        isGameActive=true;
+        gameZone();
+        setTimeout(game, timeToFrame);
     })
     
-    function game(){
+    function gameZone(){
         var div=document.getElementById('gameWindow');
-        for(var i = 0; i<20; i++){
-            for(var j = 0; j<20; j++){
-                var newDiv = document.createElement('div');
-                newDiv.id =`${i} ${j}`;
-                newDiv.classList.add('areaBox');
-                div.appendChild(newDiv);
+        if(isNotCreated === true){
+            for(var i = 0; i<21; i++){
+                for(var j = 0; j<21; j++){
+                    var newDiv = document.createElement('div');
+                    newDiv.id =`${i} ${j}`;
+                    newDiv.classList.add('areaBox');
+                    div.appendChild(newDiv);
+                    if(i === 20){
+                        isNotCreated = false;
+                    }
+                }  
             }
+        }   
+    }  
+        
+    }
+
+    function addPlatform(){
+        for(var i = 8; i < 13; i++){
+            document.getElementById(19+' '+`${i}`).classList.add('platform');
+        }
+        
+    }
+
+    function addBall(){
+        document.getElementById(18+' '+10).classList.add('ball');
+    }
+
+    function addBlock(){
+        for(var i = 2; i < 8; i++){
+            for(var j = 2; j < 19; j++){
+                document.getElementById(`${i}`+' '+`${j}`).classList.add('block');
+            }
+
         }
     }
-}
+
+    function game(){
+        addPlatform()
+        addBall();
+        addBlock();
+        if(isGameActive) setTimeout(game, timeToFrame);
+    }
+
+    
